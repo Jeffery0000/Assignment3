32296cb79f2d5254be8bf36a92cc605473f93ea2
Jeffery Lin <jeffery.lin@student.tdsb.on.ca>
2025-03-07 15:04:28 -0500

    Created ambiguous case function.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const heronsFormula = (a, b, c) => {
-    return (1/4) * Math.sqrt((4 * a * a * b * b) - (Math.pow(((a * a) + (b * b) - (c * c)), 2)));
+    return (1 / 4) * Math.sqrt((4 * a * a * b * b) - (Math.pow(((a * a) + (b * b) - (c * c)), 2)));
+}
+
+const ambiguousCase = (angleA, sideA, sideB) => {
+    
 }
