c8b689b1222ed36f9fd8216b31174767d0926f31
Jeffery Lin <jeffery.lin@student.tdsb.on.ca>
2025-03-23 17:44:03 -0400

    Fixed some variables and formatting.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const heronsFormula = (a, b, c) => {
 
 document.getElementById("heron-form").addEventListener("submit", (event) => {
     event.preventDefault();
+
     const a = document.getElementById("heron-a").value;
     const b = document.getElementById("heron-b").value;
     const c = document.getElementById("heron-c").value;
     let area = heronsFormula(a, b, c);
+
     area = Math.round(area * 100) / 100;
     document.getElementById("heron-result").value = area;
 });
@@ -15,65 +17,70 @@ document.getElementById("heron-form").addEventListener("submit", (event) => {
 const ambiguousCase = (angleA, sideA, sideB) => {
     let height = sideB * Math.sin(angleA * (Math.PI / 180));
     height = Math.round(height * 100) / 100;
-    sideA = Math.round(sideA * 100) / 100;
-    console.log(height);
+    const sideARounded = Math.round(sideA * 100) / 100;
+
     if (angleA <= 90) {
-        if (sideA < height) {
-            document.getElementById("ambiguous-result").value = "no triangle";
+        if (sideARounded < height) {
+            return "no triangle";
         }
-        else if (sideA === height) {
-            document.getElementById("ambiguous-result").value = "right triangle";
+        else if (sideARounded === height) {
+            return "right triangle";
         }
-        else if (sideA > height) {
-            document.getElementById("ambiguous-result").value = "one triangle";
+        else if (sideARounded > height) {
+            return "one triangle";
         }
-        else if (height < sideA && sideA < sideB) {
-            document.getElementById("ambiguous-result").value = "two triangles";
+        else if (height < sideARounded && sideARounded < sideB) {
+            return "two triangles";
         }
     }
     else if (angleA < 180) {
-        if (sideA < sideB || sideA === sideB) {
-            document.getElementById("ambiguous-result").value = "no triangle";
+        if (sideARounded < sideB || sideARounded === sideB) {
+            return "no triangle";
         }
-        else if (sideA > sideB) {
-            document.getElementById("ambiguous-result").value = "one triangle";
+        else if (sideARounded > sideB) {
+            return "one triangle";
         }
     }
     else {
-        document.getElementById("ambiguous-result").value = "no triangle";
+        return "no triangle";
     }
 }
 
 document.getElementById("ambiguous-form").addEventListener("submit", (event) => {
     event.preventDefault();
+
     const angleA = parseFloat(document.getElementById("ambiguous-angle-a").value);
     const sideA = parseFloat(document.getElementById("ambiguous-side-a").value);
     const sideB = parseFloat(document.getElementById("ambiguous-side-b").value);
-    ambiguousCase(angleA, sideA, sideB);
+
+    document.getElementById("ambiguous-result").value = ambiguousCase(angleA, sideA, sideB);
 });
 
-const newtonFunction = (x) => {
-    return (6 * Math.pow(x, 4)) - (13 * Math.pow(x, 3)) - (18 * Math.pow(x, 2)) + (7 * x) + 6;
-}
-
-const newtonDerivative = (x) => {
-    return (24 * Math.pow(x, 3)) - (39 * Math.pow(x, 2)) - (36 * x) + 7;
-}
-
 const newtonsMethod = (guess) => {
+
+    const newtonFunction = (x) => {
+        return (6 * Math.pow(x, 4)) - (13 * Math.pow(x, 3)) - (18 * Math.pow(x, 2)) + (7 * x) + 6;
+    }
+
+    const newtonDerivative = (x) => {
+        return (24 * Math.pow(x, 3)) - (39 * Math.pow(x, 2)) - (36 * x) + 7;
+    }
+
     let x1 = guess;
 
     do {
         x1 = x1 - (newtonFunction(x1)) / (newtonDerivative(x1));
     } while (Math.abs(newtonFunction(x1)) > 0.0001);
+
     return Math.round(x1 * 100) / 100;
 }
 
 document.getElementById("newtons-form").addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("newton");
+
     const guess = parseFloat(document.getElementById("root-guess").value);
     const rootApprox = newtonsMethod(guess);
+
     document.getElementById("root-approx").value = rootApprox;
 });
 
